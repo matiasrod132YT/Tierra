7234c9b819d2bed785c01c0a8426dd2b831851d7
Matias <matiasrodriesc@gmail.com>
2023-02-04 21:26:40 -0500

    test

--- Comandos/Publico/invitaciones.js
+++ Comandos/Publico/invitaciones.js
@@ -12,18 +12,18 @@ module.exports = {
 
     async execute(interaction, client) {
         const usuario = interaction.options.getUser("usuario");
-          let invites = await interaction.guild.invites.fetch();
-          let userInv = invites.filter(u => u.inviter && u.inviter.id === usuario.id);
+        let invites = await interaction.guild.invites.fetch();
+        let userInv = invites.filter(u => u.inviter && u.inviter.id === usuario.id);
 
-          let i = 0;
+        let i = 0;
 
-          userInv.forEach(inv => i += inv.uses);
+        userInv.forEach(inv => i += inv.uses);
 
 
-          const embed = new EmbedBuilder()
-          .setTitle(`👥 | INVITACIONES`)
-          .setDescription(`**${usuario}** tiene \`${i}\` invitaciones`)
-          .setColor(client.config.prefix)
+        const embed = new EmbedBuilder()
+        .setTitle(`👥 | INVITACIONES`)
+        .setDescription(`**${usuario}** tiene \`${i}\` invitaciones`)
+        .setColor(client.config.prefix)
 
         interaction.reply({ embeds: [embed] });
     }
